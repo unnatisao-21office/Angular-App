854eeb612de178c262879c59fb821ebffd0b8ee3
Unnati Sao <unnati.sao@cbnits.com>
2025-12-31 12:18:18 +0530

    propertybinding-eventbinding

--- src/app/pages/data-binding/data-binding.ts
+++ src/app/pages/data-binding/data-binding.ts
@@ -10,4 +10,17 @@ export class DataBinding {
    courseName: string = "Angular Full Course";
    courseDuration : string = "2hrs";
    isActive: boolean = true;
+   myClassName: string = "bg-success text-white p-2";
+   myInputType = "checkbox";
+
+   showAlert() {
+    alert("Welcome to angular")
+   }
+   changeCourseName(name: string){
+    this.courseName = name;
+   }
+   onCityChange(event: any){
+    console.log(event.target.value);
+    alert("You have selected: " + event.target.value);//property binding
+   }
 }
