2098b535d027f24a6012ff4885385852427c1250
Unnati Sao <unnati.sao@cbnits.com>
2026-01-05 12:02:20 +0530

    resolved the issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { User } from './pages/user/user';
 import { DataBinding } from './pages/data-binding/data-binding';
 import { Controlflow } from './pages/controlflow/controlflow';
+import { DynamicCssClass } from './pages/dynamic-css-class/dynamic-css-class';
+import { Emplyoee } from './pages/emplyoee/emplyoee';
 export const routes: Routes = [
     
     {
@@ -15,6 +17,14 @@ export const routes: Routes = [
     {
         path:'control-flow-page',
         component: Controlflow
+    },
+    {
+        path:'dynamic-css',
+        component: DynamicCssClass
+    },
+    {
+        path:'employee-page',
+        component: Emplyoee
     }
 
 
