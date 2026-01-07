982be9d61dcf822a9b448aac06108ef6c7976b86
Unnati Sao <unnati.sao@cbnits.com>
2026-01-07 11:20:37 +0530

    added service file in user.ts

--- src/app/pages/user/user.ts
+++ src/app/pages/user/user.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Master } from '../../services/master';
+import { IEmployee } from '../../model/Employee.model';
 @Component({
   selector: 'app-user',
   imports: [],
@@ -10,21 +11,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class User implements OnInit  {
   http = inject(HttpClient);
-  userList: any[] =[];
+  master = inject(Master); //injecting service using inject function
+  userList = signal<IEmployee[]>([]);
   departmentArray: any[]=[];
   ngOnInit(): void {
     this.getUser();
     this.getDept();
+  
   }
 
   getUser(){
-    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
+   
+    this.master.getUser().subscribe((res:any)=>{
       this.userList = res;
-    })
+    });
   }
 
   getDept(){
-    this.http.get("https://api.freeprojectapi.com/api/EmployeeApp/GetDepartments").subscribe((res:any)=>{
+    this.master.getDept().subscribe((res:any)=>{
       this.departmentArray = res;
     });
   }
